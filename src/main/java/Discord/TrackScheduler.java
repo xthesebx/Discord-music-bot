df2080cafd78b9f18ec4d8bb9df5d1581adbf75f
xXTheSebXx <sebastianjerke@gmail.com>
2024-05-25 20:37:10 +0200

    fix skip command clearing queue

--- src/main/java/Discord/TrackScheduler.java
+++ src/main/java/Discord/TrackScheduler.java
@@ -4,7 +4,6 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
-import net.dv8tion.jda.api.managers.AudioManager;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -48,7 +47,7 @@ public class TrackScheduler extends AudioEventAdapter {
 	}
 	
 	/**
-	 * Start the next track, stopping the current one if it is playing.
+	 * Start the next track, skipping the current one if it is playing.
 	 */
 	public void nextTrack() {
 		// Start the next track, regardless of if something is already playing or not. In case queue was empty, we are
@@ -57,7 +56,6 @@ public class TrackScheduler extends AudioEventAdapter {
 		player.startTrack(track, false);
 		if (track == null) {
 			server.getDc().startTimer();
-			//map.get(id).put("dcThread", dcThread);
 		}
 	}
 	
@@ -67,6 +65,8 @@ public class TrackScheduler extends AudioEventAdapter {
 		// Only start the next track if the end reason is suitable for it (FINISHED or LOAD_FAILED)
 		if (endReason.mayStartNext) {
 			nextTrack();
+		} else if (endReason.equals(AudioTrackEndReason.REPLACED)) {
+			return;
 		} else {
 			queue.clear();
 		}
