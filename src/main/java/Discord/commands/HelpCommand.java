d840be94ade0e7bc0ef8bf84b5f830e60119caf7
xXTheSebXx <sebastianjerke@gmail.com>
2024-06-05 23:28:44 +0200

    update help text

--- src/main/java/Discord/commands/HelpCommand.java
+++ src/main/java/Discord/commands/HelpCommand.java
@@ -23,15 +23,17 @@ public class HelpCommand extends BasicCommand {
         event.reply("""
 							```Command List:
 							/play [name or link] - Play a song from youtube with link or search by name
-							/bind - bind the discord bot to this text channel
-							/unbind - unbind the discord bot from text channels
 							/join - Join your current voice Channel (must be in voice Channel for this)
 							/leave - leave the current voice Channel
 							/pause - pause the current music
 							/resume - resume the current music
 							/stop - stops the music and empties the queue
 							/info - shows current song and time in song
-							/queue - show queue```""").queue();
+							/queue - show queue
+							/repeat - repeats the queue
+							/skip - skips the current song
+							/volume - sets the volume
+							/shuffle - shuffles the queue```""").queue();
     }
 }
 
