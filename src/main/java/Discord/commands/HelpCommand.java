5a8f2af94d7777343f2047aa658292a60f2a948c
xXTheSebXx <sebastianjerke@gmail.com>
2025-02-07 13:03:02 +0100

    update help text

--- src/main/java/Discord/commands/HelpCommand.java
+++ src/main/java/Discord/commands/HelpCommand.java
@@ -33,9 +33,14 @@ public class HelpCommand extends BasicCommand {
 							/queue - show queue
 							/repeat - repeats the queue
 							/skip - skips the current song
+							/previous - plays the previous song
 							/volume - sets the volume
 							/shuffle - shuffles the queue
-							/lyrics - prints the lyrics of current song```""").queue();
+							/lyrics - prints the lyrics of current song
+							/appconnect - to connect to the app
+							for the app go to https://github.com/xthesebx/musicbot-app/releases/latest 
+							download the zip, extract it and execute the .exe
+							/streamermode - for connection with the twitch chat for requests```""").queue();
     }
 }
 
