ffa4ecead85a962f555b3972c003b916eb97565a
xXTheSebXx <sebastianjerke@gmail.com>
2025-04-24 17:49:50 +0200

    fix current bot issue

--- src/main/java/Discord/ShutdownHook.java
+++ src/main/java/Discord/ShutdownHook.java
@@ -1,6 +1,8 @@
 package Discord;
 
 
+import Discord.App.AppInstance;
+
 /**
  * <p>ShutdownHook class.</p>
  *
@@ -23,9 +25,7 @@ public class ShutdownHook implements Runnable {
     @Override
     public void run() {
         for (String s : main.map.keySet()) {
-            main.map.get(s).getAppInstances().forEach(instance -> {
-                instance.close();
-            });
+            main.map.get(s).getAppInstances().forEach(AppInstance::close);
         }
     }
 }
